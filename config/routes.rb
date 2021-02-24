f145d0f458de0619f847ba7fbfa2c5a28e4961ab
Dinara9595 <dinara.petuhova95@yandex.ru>
2021-02-24 08:00:59 +0300

    Finish app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
+  resources :microposts
+  resources :users
   get "/articles", to: "articles#index"
-  root "articles#index"
+  root "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
